02c64e5ef6816bdab03e6bdffba5d68aaa63eacf
Shachar Ovadya <ovadya.shachar@gmail.com>
2022-03-11 07:32:33 +0200

    create IApiResponse, IStudent properties

--- src/app/services/apis/apis-interfaces.ts
+++ src/app/services/apis/apis-interfaces.ts
@@ -1,3 +1,10 @@
+export interface IApiResponse<TData = any> {
+    data: TData;
+}
+
 export interface IStudent {
-    
+    id?: number;
+    name?: string;
+    birthDate?: Date;
+    group?: string;
 }
