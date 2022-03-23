e1a05f06b7d82b43cbc1dd398ecfa1991678056e
MacroLove <3049720393@qq.com>
2022-03-23 18:03:05 +0800

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const chromium = require('chromium');
+process.env.CHROMIUM_BIN = chromium.path;
 
 module.exports = function (config) {
   config.set({
